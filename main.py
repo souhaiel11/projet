88fc494d89712795921d05d9ea482211255014d9
amri souhaiel <amrisouhail96@gmail.com>
2024-02-19 19:49:15 +0100

    main

---
+++ main.py
@@ -0,0 +1,16 @@
+from souhaiel import GestionListe
+
+def main():
+    ma_liste = ["a", "b", "c", "d", "e"]
+    gestion_liste = GestionListe(ma_liste)
+
+    print("Affichage initial de la liste :")
+    gestion_liste.afficher_liste()
+
+    gestion_liste.modifier_liste(2, "z")
+
+    print("\nAffichage de la liste après modification :")
+    gestion_liste.afficher_liste()
+
+if __name__ == "__main__":
+    main()
