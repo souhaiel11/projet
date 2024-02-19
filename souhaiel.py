32edb65b850b9e538f61e3014484d3fd5cb42d58
amri souhaiel <amrisouhail96@gmail.com>
2024-02-19 19:41:50 +0100

    souhaiel

--- souhaiel.py
+++ souhaiel.py
@@ -1,15 +1,15 @@
 class GestionListe:
-    def init(animal, ma_liste):
-        animal.ma_liste = ma_liste
+    def __init__(self, ma_liste):
+        self.ma_liste = ma_liste
 
-    def afficher_liste(animal):
+    def afficher_liste(self):
         print("Contenu de la liste :")
-        for element in animal.ma_liste:
+        for element in self.ma_liste:
             print(element)
 
-    def modifier_liste(animal, index, nouvel_element):
-        if 0 <= index < len(animal.ma_liste):
-            animal.ma_liste[index] = nouvel_element
+    def modifier_liste(self, index, nouvel_element):
+        if 0 <= index < len(self.ma_liste):
+            self.ma_liste[index] = nouvel_element
             print("Élément à l'index {} modifié avec succès.".format(index))
         else:
             print("Index invalide. Impossible de modifier l'élément.")
